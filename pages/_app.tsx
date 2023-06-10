d1624bd8140347807be218eca2bd4ff399136df6
Abdulaziz Al-Homaid <ahomaidcmuq@gmail.com>
2023-06-10 17:23:57 +0300

    First commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.css'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useEffect } from "react";
+
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    require("bootstrap/dist/css/bootstrap.css");
+  }, []);
+
   return <Component {...pageProps} />
 }
